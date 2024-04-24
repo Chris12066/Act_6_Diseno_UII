7fdf8225f1a4d1ecb983b0dc37c385407b633bfc
CHRISTIAN_ALEXIS_ALCANTARA_GONZALEZ <a.21308051280308@cbtis128.edu.mx>
2024-04-24 15:27:16 +0000

    Act

---
+++ lib/data/product_data.dart
@@ -0,0 +1,19 @@
+import 'package:act_6_diseno_ui/models/product_model.dart';
+
+List<ProductModel> products = [
+  ProductModel(
+      img: "assets/images/180sx.jpg",
+      title: "Nissan 180SX",
+      description:
+          "El Nissan 180SX es un automóvil fastback que Nissan Motors produjo entre 1988 y 1998."),
+  ProductModel(
+      img: "assets/images/nsx.jpg",
+      title: "Honda NSX",
+      description:
+          "El Honda NSX (abreviatura de New Sport eXperimental), es un automóvil deportivo de dos puertas biplaza con motor central-trasero, producido por fabricante japonés Honda entre 1990 y 2005."),
+  ProductModel(
+      img: "assets/images/rx7.jpg",
+      title: "Mazda RX-7",
+      description:
+          "El Mazda RX-7 es un automóvil deportivo producido por el fabricante japonés Mazda Motor Corporation (マツダ株式会社 Matsuda Kabushiki-gaisha?) entre los años 1978 y 2002."),
+];
